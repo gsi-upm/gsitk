197dc83d3abef0726ec7f2dc6a53bd91d587904c
Oscar Araque <oscar.aiborra@gmail.com>
2017-10-18 12:30:31 +0200

    Minor setup.py fix

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name = 'gsitk',
       author = 'Oscar Araque',
       email = 'oscar.aiborra@gmail.com',
       url = 'https://github.com/gsi-upm/gsitk', # URL to the github repo
-      download\_url = 'https://github.com/gsi-upm/gsitk/tarball/0.1',
+      download_url = 'https://github.com/gsi-upm/gsitk/tarball/0.1',
       keywords = ['sentiment analysis', 'nlp', 'machine learning'], # list of keywords that represent your package
       classifiers = [], )
