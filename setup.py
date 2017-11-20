7199f066999dfb107a09d908a31fb285335d1db6
Oscar Araque <oscar.aiborra@gmail.com>
2017-11-20 17:21:03 +0100

    Final packaging details

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import pip
 from setuptools import setup
 from pip.req import parse_requirements
 
+VERSION = '0.1.5'
+
+
 try:
     install_reqs = parse_requirements(
         "requirements.txt", session=pip.download.PipSession())
@@ -14,12 +17,13 @@ install_reqs = [str(ir.req) for ir in install_reqs]
 
 setup(name = 'gsitk',
       packages = ['gsitk'], # this must be the same as the name above
-      version = '0.1.2',
+      version = VERSION,
       description = 'gsitk is a library on top of scikit-learn that eases the development process on NLP machine learning driven projects.',
       author = 'Oscar Araque',
       email = 'oscar.aiborra@gmail.com',
       url = 'https://github.com/gsi-upm/gsitk', # URL to the github repo
-      download_url = 'https://github.com/gsi-upm/gsitk/tarball/0.1.2',
+      download_url =
+      'https://github.com/gsi-upm/gsitk/tarball/{}'.format(VERSION),
       keywords = ['sentiment analysis', 'nlp', 'machine learning'], # list of keywords that represent your package
       classifiers = [],
       install_requires=install_reqs,
